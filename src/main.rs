9320c5ff9b8743a50be29a06ed0c507ce47d0c25
Dan Miller <dan.miller@fraculation.com>
2024-04-07 18:03:21 -0500

    maximum nesting depth

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod is_isomorphic;
 mod length_of_last_word;
 mod word_search;
+mod n_1614_maximum_nesting_depth_of_the_parentheses;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/n_1614_maximum_nesting_depth_of_the_parentheses.rs
@@ -0,0 +1,36 @@
+struct Solution{}
+
+impl Solution {
+
+    pub fn max_depth(s: String) -> i32 {
+        let mut depth = 0;
+        let mut max_depth = 0;
+        for char in s.chars() {
+            depth += match char {
+                '(' => 1,
+                ')' => -1,
+                _ => 0
+            };
+            max_depth = max_depth.max(depth);
+        }
+
+        max_depth
+    }
+}
+
+mod test {
+
+    use super::*;
+    #[test]
+    fn test_1(){
+        assert_eq!(Solution::max_depth("()(())".to_string()), 2);
+    }
+    #[test]
+    fn test_2(){
+        assert_eq!(Solution::max_depth("".to_string()), 0);
+    }
+    #[test]
+    fn test_3(){
+        assert_eq!(Solution::max_depth("()()".to_string()), 1);
+    }
+}
