12d1bcbbeb7f8148b48eab5b675d7458117f9a51
Dan Miller <dan.miller@fraculation.com>
2024-04-07 17:48:35 -0500

    word search impl that works. 102ms, beats 62% runtime, 86% memory

--- src/word_search.rs
+++ src/word_search.rs
@@ -2,22 +2,23 @@ use std::ops::{Index, IndexMut};
 
 struct Solution{}
 
+
 impl Solution {
     pub fn exist(board: Vec<Vec<char>>, word: String) -> bool {
         let x_size = board.len();
         let y_size = board[0].len();
         let mut visited_map = (0..x_size).map(
-            |_| vec![0; y_size]
+            |_| vec![false; y_size]
         ).collect();
 
         let char_vec: Vec<char> = word.chars().collect();
         let char_slice = char_vec.as_slice();
 
-        let mut search_index = 0;
+        let mut search_attempt_index = 0;
         for x in 0..x_size {
             for y in 0..y_size {
-                search_index += 1;
-                if Self::search_from_cell(&board, search_index, x, y, &mut visited_map, char_slice) {
+                search_attempt_index += 1;
+                if Self::search_from_cell(&board, search_attempt_index, x, y, &mut visited_map, char_slice) {
                     return true;
                 }
             }
@@ -32,7 +33,7 @@ impl Solution {
         search_index: usize,
         cell_x: usize,
         cell_y: usize,
-        visited: &mut Vec<Vec<usize>>,
+        visited: &mut Vec<Vec<bool>>,
         word: &[char]) -> bool {
 
         if word.len() == 0 { return true }
@@ -49,24 +50,24 @@ impl Solution {
             return false;
         };
 
-        if *visited_cell == search_index {
+        if *visited_cell {
             return false;
         }
 
-        *visited_cell = search_index;
+        *visited_cell = true;
 
         let (_, new_word) = word.split_at(1);
 
-        if
+        let did_any_branch_match =
                           Self::search_from_cell(board, search_index, cell_x + 1, cell_y, visited, new_word) ||
             cell_x > 0 && Self::search_from_cell(board, search_index, cell_x - 1, cell_y, visited, new_word) ||
                           Self::search_from_cell(board, search_index, cell_x, cell_y + 1, visited, new_word) ||
-            cell_y > 0 && Self::search_from_cell(board, search_index, cell_x, cell_y - 1, visited, new_word)
-        {
-            return true;
-        }
+            cell_y > 0 && Self::search_from_cell(board, search_index, cell_x, cell_y - 1, visited, new_word);
 
-        false
+        // on unwind, reset visited matrix
+        visited[cell_x][cell_y] = false;
+
+        did_any_branch_match
     }
 }
 
@@ -96,7 +97,6 @@ impl<T> IndexMut<Vector2D> for Vec2DWrapper<T>{
     }
 }
 
-
 impl<T> Vec2DWrapper<T>{
     fn new(data: Vec<Vec<T>>) -> Vec2DWrapper<T> {
         // TODO: validate non-jagged?
@@ -123,17 +123,22 @@ impl<T> Vec2DWrapper<T> where T : Copy{
 }
 
 mod test {
-
     use super::*;
     #[test]
     fn test_1(){
-        let board = vec![vec!['A','B','C','E'],vec!['S','F','C','S'],vec!['A','D','E','E']];
+        let board = vec![
+            vec!['A','B','C','E'],
+            vec!['S','F','C','S'],
+            vec!['A','D','E','E']];
         let word = "ABCCED".to_string();
         assert_eq!(Solution::exist(board, word), true);
     }
     #[test]
     fn test_2(){
-        let board = vec![vec!['A','B','C','E'],vec!['S','F','E','S'],vec!['A','D','E','E']];
+        let board = vec![
+            vec!['A','B','C','E'],
+            vec!['S','F','E','S'],
+            vec!['A','D','E','E']];
         let word = "ABCESEEEFS".to_string();
         assert_eq!(Solution::exist(board, word), true);
     }
