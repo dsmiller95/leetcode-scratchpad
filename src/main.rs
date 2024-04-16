650233bc0f1fdeffa9ad18e192383a8f126601e5
Dan Miller <dan.miller@fraculation.com>
2024-04-16 10:38:18 -0500

    copy pasta new problem

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ mod length_of_last_word;
 mod word_search;
 mod n_1614_maximum_nesting_depth_of_the_parentheses;
 mod n42_trapping_rain_water;
+mod n_623_add_one_row_to_tree;
 
 fn main() {
     println!("Hello, world!");
