b7c7f9aa4b56c1b67017568042aeee9cede3805e
Dan Miller <dsmiller95@gmail.com>
2024-05-06 11:03:20 -0500

    Impl utilities for linkedlist

--- src/n_2487_remove_nodes_from_linked_list.rs
+++ src/n_2487_remove_nodes_from_linked_list.rs
@@ -1,4 +1,5 @@
 use core::panic;
+use std::borrow::{Borrow, BorrowMut};
 
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
@@ -12,21 +13,33 @@ impl ListNode {
     fn new(val: i32) -> Self {
         ListNode { next: None, val }
     }
+}
+struct Solution();
+
+impl ListNode {
+    fn reverse(node: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut vec_vals = Self::to_vec(node);
+        vec_vals.reverse();
+        Self::from_vec(vec_vals)
+    }
 
     fn from_vec(vals: Vec<i32>) -> Option<Box<Self>> {
         if vals.is_empty() {
             return None;
         }
 
-        let mut nodes = vals.iter().map(|&x| Self::new(x)).collect::<Vec<_>>();
-        for i in 0..(nodes.len() - 1) {
-            let next = nodes[i + 1].clone();
-            let mut current = nodes[i].clone();
-            current.next = Some(Box::new(next));
-            nodes[i] = current;
+        let mut nodes = vals
+            .iter()
+            .map(|&x| Some(Box::new(Self::new(x))))
+            .collect::<Vec<_>>();
+
+        for i in (1..(vals.len())).rev() {
+            let next_node = nodes[i].take().unwrap();
+            let prev_nod = nodes[i - 1].borrow_mut().as_mut().unwrap();
+            prev_nod.next = Some(next_node);
         }
 
-        Some(Box::new(nodes[0].clone()))
+        nodes[0].clone()
     }
 
     fn to_vec(node: Option<Box<ListNode>>) -> Vec<i32> {
@@ -40,10 +53,11 @@ impl ListNode {
         result
     }
 }
-struct Solution();
 
 impl Solution {
     pub fn remove_nodes(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let reversed = ListNode::reverse(head);
+
         todo!()
     }
 }
@@ -74,5 +88,27 @@ mod tests {
         let result = Solution::remove_nodes(list);
         let result_vec = ListNode::to_vec(result);
         assert_eq!(expected, result_vec);
+    }
+
+    #[test]
+    fn test_reverse() {
+        let list = [2, 4, 56, 9, 9, 2];
+        let expected = [2, 9, 9, 56, 4, 2];
+
+        let list = ListNode::from_vec(list.to_vec());
+        let expected = expected.to_vec();
+        let result = ListNode::reverse(list);
+        let result_vec = ListNode::to_vec(result);
+        assert_eq!(expected, result_vec);
+    }
+
+    #[test]
+    fn test_to_vec_round_trip() {
+        let list = [2, 4, 56, 9, 9, 2];
+
+        let list_head = ListNode::from_vec(list.to_vec());
+        let expected = list.to_vec();
+        let result = ListNode::to_vec(list_head);
+        assert_eq!(expected, result);
     }
 }
