12bd87b704e720c4331aa8d70214e8b3b20ece08
Dan Miller <dsmiller95@gmail.com>
2024-05-07 11:51:17 -0500

    Initial attemt
    38ms, 2.79MB. beats 50% runtime, 100% memory
    
    not doing my own tests this time. way too much work to re-write that
    boilerplate with rust

--- src/n_2816_double_number_linked_list.rs
+++ src/n_2816_double_number_linked_list.rs
@@ -15,6 +15,42 @@ struct Solution();
 
 impl Solution {
     pub fn double_it(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-        todo!()
+        let mut head = head;
+        let mut rev_head = None;
+
+        while let Some(mut node) = head {
+            let next = node.next.take();
+            node.next = rev_head;
+
+            rev_head = Some(node);
+            head = next;
+        }
+
+        let mut carry = 0;
+
+        (head, rev_head) = (rev_head, head);
+
+        while let Some(mut node) = head {
+            let next = node.next.take();
+            node.next = rev_head;
+
+            let full_val = node.val * 2 + carry;
+            carry = full_val / 10;
+            node.val = full_val % 10;
+
+            rev_head = Some(node);
+            head = next;
+        }
+        (head, rev_head) = (rev_head, head);
+
+        if carry != 0 {
+            let new_node = ListNode {
+                next: head,
+                val: carry,
+            };
+            head = Some(Box::new(new_node));
+        }
+
+        head
     }
 }
